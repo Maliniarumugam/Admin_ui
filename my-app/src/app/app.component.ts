ea2865bb2e97c2e0e74723311bb9b79131d8fb96
Maliniarumugam <malinisangeetha2000@gmail.com>
2026-06-15 17:33:13 +0530

    UI changes

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-app';
+  data = [
+    { role: 'Admin', accessControlType: 'Full Access', caseCategory: 'General', caseSubcategory: 'Inquiry' },
+    { role: 'User', accessControlType: 'Read Only', caseCategory: 'Support', caseSubcategory: 'Technical' },
+    { role: 'User', accessControlType: 'Read Only', caseCategory: 'Support', caseSubcategory: 'Technical' },
+    { role: 'User', accessControlType: 'Read Only', caseCategory: 'Support', caseSubcategory: 'Technical' },
+    { role: 'User', accessControlType: 'Read Only', caseCategory: 'Support', caseSubcategory: 'Technical' },
+    { role: 'User', accessControlType: 'Read Only', caseCategory: 'Support', caseSubcategory: 'Technical' },
+    { role: 'User', accessControlType: 'Read Only', caseCategory: 'Support', caseSubcategory: 'Technical' },
+    { role: 'User', accessControlType: 'Read Only', caseCategory: 'Support', caseSubcategory: 'Technical' },
+    { role: 'User', accessControlType: 'Read Only', caseCategory: 'Support', caseSubcategory: 'Technical' }
+  ];
+}
